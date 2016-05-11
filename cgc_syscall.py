24bced5ec51b957c292f6ba35dcf98ec50a3ee12
0xddaa <0xddaa@gmail.com>
2016-05-11 22:12:20 +0800

    change function name

--- cgc_syscall.py
+++ cgc_syscall.py
@@ -15,27 +15,39 @@ syscall_table = {
     7: 'random'
 }
 
-# get instruction range
-for s in idautils.Segments():
-    if idc.SegName(s) == '.text':
-        start_ea = idc.SegStart(s)
-        end_ea = idc.SegEnd(s)
+def use_libcgc():
+    return True
 
-if debug:
-    print 'Start: {}, End: {}'.format(hex(start_ea).strip('L'), hex(end_ea).strip('L'))
+def get_inst_range():
+    # get instruction range
+    for s in idautils.Segments():
+        if idc.SegName(s) == '.text':
+            start_ea = idc.SegStart(s)
+            end_ea = idc.SegEnd(s)
 
-# visit all instructions and fix comment for syscall instruction
-eax = -1
-ip = start_ea
-while ip < end_ea and ip != idaapi.BADADDR:
-    if 'int' in idc.GetMnem(ip) and '80h' == idc.GetOpnd(ip, 0):
-        if eax != -1:
-            idc.MakeComm(ip, 'CGC syscall: {}'.format(syscall_table[eax]))
-    elif 'mov' in idc.GetMnem(ip) and 'eax' == idc.GetOpnd(ip, 0) and 5 == idc.GetOpType(ip, 1):
-        value = idc.GetOpnd(ip, 1)
-        if re.search('^[0-9]+$', value) != None:
-            eax = int(value)
-        if eax > 7 or eax < 1:
-            eax = -1
-        
-    ip = idc.NextHead(ip)
+    if debug:
+        print 'Start: {}, End: {}'.format(hex(start_ea).strip('L'), hex(end_ea).strip('L'))
+
+    return start_ea, end_ea
+
+def change_comment():
+    # visit all instructions and fix comment for syscall instruction
+    start_ea, end_ea = get_inst_range()
+    eax = -1
+    ip = start_ea
+    while ip < end_ea and ip != idaapi.BADADDR:
+        if 'int' in idc.GetMnem(ip) and '80h' == idc.GetOpnd(ip, 0):
+            if eax != -1:
+                idc.MakeComm(ip, 'CGC syscall: {}'.format(syscall_table[eax]))
+                if use_libcgc():
+                    idc.MakeName(idc.GetFunctionAttr(ip, FUNCATTR_START), syscall_table[eax])
+        elif 'mov' in idc.GetMnem(ip) and 'eax' == idc.GetOpnd(ip, 0) and 5 == idc.GetOpType(ip, 1):
+            value = idc.GetOpnd(ip, 1)
+            if re.search('^[0-9]+$', value) != None:
+                eax = int(value)
+            if eax > 7 or eax < 1:
+                eax = -1
+
+        ip = idc.NextHead(ip)
+
+change_comment()
