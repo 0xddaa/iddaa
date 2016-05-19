e1e8165295983c18b141b84d8c050bd199946fcf
0xddaa <0xddaa@gmail.com>
2016-05-19 22:28:57 +0800

    add cgc to idapythonrc

--- idapythonrc.py
+++ idapythonrc.py
@@ -1,6 +1,11 @@
 import sys
 sys.path.append('E:\iddaa')
 import iddaautils as utils
+import iddaacgc
 
+# start sync server
 gdbsync = utils.GDBSync()
 gdbsync.start()
+
+# init cgc
+cgchelper = CGCHelper()
--- iddaacgc.py
+++ iddaacgc.py
@@ -1,5 +1,5 @@
 import idaapi
-import idautils
+import idautils as utils
 import idc
 import re
 
@@ -16,24 +16,9 @@ syscall_table = {
 }
 
 class CGCHelper:
-    def __use_libcgc(self):
-        return True
-
-    def __get_inst_range(self):
-        # get instruction range
-        for s in idautils.Segments():
-            if idc.SegName(s) == '.text':
-                start_ea = idc.SegStart(s)
-                end_ea = idc.SegEnd(s)
-
-        if debug:
-            print 'Start: {}, End: {}'.format(hex(start_ea).strip('L'), hex(end_ea).strip('L'))
-
-        return start_ea, end_ea
-
-    def revise_syscall(self):
+    def revise_syscall(self, rename=False):
         # visit all instructions
-        start_ea, end_ea = self.__get_inst_range()
+        start_ea, end_ea = utils.get_seg_range('.text')
         eax = -1
         ip = start_ea
         while ip < end_ea and ip != idaapi.BADADDR:
@@ -41,7 +26,7 @@ class CGCHelper:
                 if eax != -1:
                     # fix comment and function name
                     idc.MakeComm(ip, 'CGC syscall: {}'.format(syscall_table[eax]))
-                    if self.__use_libcgc():
+                    if rename:
                         idc.MakeName(idc.GetFunctionAttr(ip, FUNCATTR_START), syscall_table[eax])
             elif 'mov' in idc.GetMnem(ip) and 'eax' == idc.GetOpnd(ip, 0) and 5 == idc.GetOpType(ip, 1):
                 value = idc.GetOpnd(ip, 1)
