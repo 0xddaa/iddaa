99e6735d34da40cf8c25a1e990a5b0629b891637
0xddaa <0xddaa@gmail.com>
2016-05-28 11:01:31 +0800

    release

--- gggdb/gggdb.py
+++ gggdb/gggdb.py
@@ -1,7 +1,10 @@
+from __future__ import print_function
 import gdb
 import socket
 import pickle
 import os
+import subprocess as sp
+import sys
 
 IDA_HOST = '10.113.208.101'
 PORT = 50216
@@ -14,21 +17,30 @@ def connect_ida():
         sys.stderr.write("[ERROR] {}\n".format(err))
         return None
 
-def recv(sock):
+def send(sock, buf):
+    if sys.version_info < (3, 0):
+        sock.send(buf)
+    else:
+        sock.send(bytes(buf, 'UTF-8'))
+
+def recv(sock, raw=False):
     buf = bytes()
     while True:
         tmp = sock.recv(4096)
         buf += tmp
         if not tmp:
             break
-    return buf
+    if raw:
+        return buf
+    else:
+        return buf if sys.version_info < (3, 0) else buf.decode()
 
 def get_ida_symbols():
     sock = connect_ida()
-    if not sock: return
+    if not sock: return 
 
-    sock.send(bytes('GETSYM', 'UTF-8'))
-    buf = recv(sock)
+    send(sock, 'GETSYM')
+    buf = recv(sock, True)
 
     with open('/tmp/symfile', 'wb') as f:
         f.write(buf)
@@ -39,6 +51,7 @@ def get_ida_symbols():
         print('Can\'t not receive ida symfile.')
 
 pseudo_code = dict()
+
 def get_pseudo_code(func):
     global pseudo_code
     if func in pseudo_code.keys():
@@ -47,7 +60,26 @@ def get_pseudo_code(func):
     sock = connect_ida()
     if not sock: return
 
-    sock.send(bytes('GETPSEUDOCODE {}'.format(func), 'UTF-8'))
-    code = recv(sock).decode().strip()
-    pseudo_code[func] = code
+    send(sock, 'GETPSEUDOCODE {}'.format(func))
+    code = recv(sock).strip()
+    if 'Function not found' not in code:
+        pseudo_code[func] = code
     print(pseudo_code[func])
+
+def get_local_type():
+    sock = connect_ida()
+    if not sock: return
+
+    sock.send(bytes('GETLOCALTYPE', 'UTF-8'))
+    buf = recv(sock, True)
+    local_type = pickle.loads(buf)
+    with open('/tmp/localtype.h', 'wb') as f:
+        f.write(bytes(local_type['header'], 'UTF-8'))
+    with open('/tmp/localtype.cpp', 'wb') as f:
+        f.write(bytes(local_type['source'], 'UTF-8'))
+    cwd = os.getcwd()
+    os.chdir('/tmp')
+    if sp.check_call('g++ -c -g localtype.cpp'.split(' ')) == 0:
+        gdb.execute('add-symbol-file /tmp/localtype.o 0') 
+    else:
+        print('Generate symbol file failed')
