e1b3ea606e8b1ceab2c2a9fd5df9da81d3fcccaa
0xddaa <0xddaa@gmail.com>
2016-05-29 10:43:59 +0800

    install rpcserver and module

--- install.bat
+++ install.bat
@@ -1,3 +1,5 @@
 SET iddaa=%~dp0
 copy "%iddaa%idapython\cgchelper.py" "C:\Program Files (x86)\IDA 6.8\plugins"
+copy "%iddaa%idapython\rpcserver.py" "C:\Program Files (x86)\IDA 6.8\plugins"
+xcopy "%iddaa%idapython\iddaa" "C:\Program Files (x86)\IDA 6.8\plugins\iddaa" /S /Y /I /D
 pause
